4cae321cf62557b4d6917f469656ac67e0d39ac1
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-05 17:44:41 +0300

    Add statistics data to dashboard.

--- CoreBlogSite/Controllers/DashboardController.cs
+++ CoreBlogSite/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,10 @@ namespace CoreBlogSite.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==2).Count().ToString();
+            ViewBag.v3 = c.Categories.Count().ToString();
             return View();
         }
     }

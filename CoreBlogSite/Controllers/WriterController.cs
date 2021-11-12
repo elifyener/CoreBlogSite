78f8ef638d56a2de7fb67aa9c8f41bc85de385a3
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-12 12:14:26 +0300

    Upload profile photo from file

--- CoreBlogSite/Controllers/WriterController.cs
+++ CoreBlogSite/Controllers/WriterController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using CoreBlogSite.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -63,6 +65,38 @@ namespace CoreBlogSite.Controllers
             }
             return View();
             
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer w = new Writer();
+            if(p.WriterImage!=null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newImagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newImagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newImagename;
+            }
+            w.WriterMail = p.WriterMail;
+            w.WriterName = p.WriterName;
+            w.WriterPassword = p.WriterPassword;
+            w.WriterConfirmPassword = p.WriterConfirmPassword;
+            w.WriterStatus = true;
+            w.WriterAbout = p.WriterAbout;
+
+            wm.TAdd(w);
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }

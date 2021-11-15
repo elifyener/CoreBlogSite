95784dfbc61036721cb2619715a790c8b9dc44f3
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-15 19:52:33 +0300

    Create Message class.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -24,5 +24,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<NewsLetter> NewsLetters { get; set; }
         public DbSet<BlogRating> BlogRatings { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 }

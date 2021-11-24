d96d3258311fbc13502f1be34effe84bd170fd37
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-24 21:07:35 +0300

    List message details.

---
+++ CoreBlogSite/Controllers/MessageController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogSite.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager m2m = new Message2Manager(new EfMessage2Repository());
+        public IActionResult Inbox()
+        {
+            int id = 1;
+            var values = m2m.GetInboxListByWriter(id);
+            return View(values);
+        }
+        public IActionResult MessageDetails(int id)
+        {
+            var value = m2m.TGetById(id);
+            return View(value);
+        }
+    }
+}

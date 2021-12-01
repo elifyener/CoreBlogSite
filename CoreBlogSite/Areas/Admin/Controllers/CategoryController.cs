08d3d06979184a958945509ffb3bfc248b43e660
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-12-01 23:51:12 +0300

    Pagination

--- CoreBlogSite/Areas/Admin/Controllers/CategoryController.cs
+++ CoreBlogSite/Areas/Admin/Controllers/CategoryController.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreBlogSite.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            var values = cm.GetList().ToPagedList(page, 3);
+            return View(values);
         }
     }
 }
